7535e8cc409b4de4dc5cf5cd14868dc5500a971b
GiverPlay007 <play.giver.007@gmail.com>
2021-05-24 13:46:25 -0300

    Project created

---
+++ game/src/main/java/me/giverplay/ganbatte/Ganbatte.java
@@ -0,0 +1,7 @@
+package me.giverplay.ganbatte;
+
+public final class Ganbatte {
+    protected void start() {
+        System.out.println("Estou rodando!");
+    }
+}
---
+++ game/src/main/java/me/giverplay/ganbatte/Main.java
@@ -0,0 +1,8 @@
+package me.giverplay.ganbatte;
+
+public class Main {
+    public static void main(String[] args) {
+        Ganbatte game = new Ganbatte();
+        game.start();
+    }
+}
